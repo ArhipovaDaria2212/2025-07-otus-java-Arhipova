661b1abcbdaf4376d21df92b89b737aa89fb8fe5
whiterun <daria21220042017@gmail.com>
2025-07-13 17:55:50 +0700

    add new gradle subproject and some simple examples

---
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -0,0 +1,32 @@
+package homework;
+
+import java.util.*;
+
+public class CustomerService {
+    private final TreeMap<Customer, String> customers;
+
+    public CustomerService() {
+        customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+    }
+
+    public Map.Entry<Customer, String> getSmallest() {
+        return copyEntry(customers.firstEntry());
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        Map.Entry<Customer, String> entry = customers.higherEntry(customer);
+        return copyEntry(entry);
+    }
+
+    public void add(Customer customer, String data) {
+        customers.put(customer, data);
+    }
+
+    private Map.Entry<Customer, String> copyEntry(Map.Entry<Customer, String> entry) {
+        if (entry == null) return null;
+
+        Customer key = entry.getKey();
+        Customer keyCopy = new Customer(key.getId(), key.getName(), key.getScores());
+        return new AbstractMap.SimpleImmutableEntry<>(keyCopy, entry.getValue());
+    }
+}
