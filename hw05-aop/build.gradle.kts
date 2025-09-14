701e81436220b80877005547dc9ec0f6c12c8287
whiterun <daria21220042017@gmail.com>
2025-09-14 23:23:08 +0700

    Added automatic logging functionality

---
+++ hw05-aop/build.gradle.kts
@@ -0,0 +1,31 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("com.github.johnrengelman.shadow")
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic")
+    implementation("org.ow2.asm:asm-commons")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+}
+
+tasks {
+    create<ShadowJar>("testLoggerJar") {
+        archiveBaseName.set("testLogger")
+        archiveVersion.set("")
+        archiveClassifier.set("")
+        manifest {
+            attributes(mapOf("Main-Class" to "ru.arhipova.Main",
+                "Premain-Class" to "ru.arhipova.agent.LogAgent"))
+        }
+        from(sourceSets.main.get().output)
+        configurations = listOf(project.configurations.runtimeClasspath.get())
+    }
+
+    build {
+        dependsOn("testLoggerJar")
+    }
+}
